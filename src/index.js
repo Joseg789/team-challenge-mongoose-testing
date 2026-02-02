19e672b9d07c5208d003385c6e567569244c86ef
joseg789 <joseg191.js@gmail.com>
2026-02-02 17:43:36 +0100

    terminando los test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const app = require("./app");
 const dotenv = require("dotenv").config();
+const app = require("./app");
 
 const PORT = 3002;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/test/post.test.js
+++ src/test/post.test.js
@@ -1,15 +1,14 @@
-const PostController = require("../controllers/post.controller.js");
 const request = require("supertest");
 const app = require("../app.js");
 
-//Test Post create
-
 describe("POST /create", () => {
   it("hay que crear un post correctamente", async () => {
-    const res = (await request(app).post("/create")).send({
-      title: "Hola, buenas",
-      body: "Mi primer post",
-    });
+    const body = {
+      title: "Hola, buenas222",
+      body: "Mi primer post333",
+    };
+
+    const res = await request(app).post("/create").send(body);
 
     expect(res.statusCode).toBe(201);
   });
